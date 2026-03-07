9e4f6802b14e1e60be153684c77d400b5db66e04
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-07 19:20:31 +0900

    test commit

---
+++ src/test/java/com/b1uffer/sessiontest/SessionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1uffer.sessiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
