36ff1329d7c805144f62eeea56b9025cb0011dc0
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-07 20:35:38 +0900

    create 메서드

--- src/main/java/com/b1uffer/sessiontest/auth/SessionManager.java
+++ src/main/java/com/b1uffer/sessiontest/auth/SessionManager.java
@@ -1,4 +1,17 @@
 package com.b1uffer.sessiontest.auth;
 
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SessionManager {
+    private Map<String, Session> store = new ConcurrentHashMap<>();
+
+    // 세션 생성 메서드
+    public Session create(String userId, long ttlSeconds) {
+        String id = UUID.randomUUID().toString();
+        Session session = new Session(id, userId, ttlSeconds);
+        store.put(id, session);
+        return session;
+    }
 }
