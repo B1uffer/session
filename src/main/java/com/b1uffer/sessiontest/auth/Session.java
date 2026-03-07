3c4cfc9f1860e9b8a305fd9ae1c70436c90e17bb
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-07 20:13:08 +0900

    Session class 정의

--- src/main/java/com/b1uffer/sessiontest/auth/Session.java
+++ src/main/java/com/b1uffer/sessiontest/auth/Session.java
@@ -1,4 +1,26 @@
 package com.b1uffer.sessiontest.auth;
 
 public class Session {
+    private String id; // 세션 ID
+    private String userId; // 사용자 ID
+    private long expiresAt; // 만료 시각
+
+    public Session(String id, String userId, long ttlSeconds) {
+        this.id = id;
+        this.userId = userId;
+        this.expiresAt = System.currentTimeMillis() + ttlSeconds * 1000;
+    }
+
+    public boolean isExpired() {
+        // expiresAt이 현재시간보다 작다면 만료됨
+        return System.currentTimeMillis() > expiresAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
