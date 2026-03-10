db9f4f703a8ff2123ce316a059e6114e233849f2
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-10 16:38:55 +0900

    실패 테스트코드 추가

--- src/test/java/com/b1uffer/sessiontest/BasicAuthHandlerTest.java
+++ src/test/java/com/b1uffer/sessiontest/BasicAuthHandlerTest.java
@@ -29,6 +29,7 @@ class BasicAuthHandlerTest {
         String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
         String authHeader = "Basic " + encodedCredentials;
 
+        System.out.println("credentials : " + credentials);
         System.out.println("authHeader : " + authHeader);
 
         when(userRepository.validate(username, password)).thenReturn(true);
@@ -39,5 +40,43 @@ class BasicAuthHandlerTest {
         // then
         assertTrue(result);
         verify(userRepository).validate(username, password);
+    }
+
+    @Test
+    void authenticate_fail_password() {
+        //Given
+        String username = "B1uffer";
+        String password = "password123";
+        String credentials = username + ":" + password;
+        String authHeader = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes());
+        when(userRepository.validate("B1uffer", "wrongpassword")).thenReturn(false);
+        System.out.println("credentials : " + credentials);
+
+        // when
+        boolean result = authHandler.authenticate(authHeader);
+        System.out.println("result : " + result);
+
+        // then
+        assertFalse(result);
+    }
+
+    @Test
+    void authenticate_fail_header() {
+        // 필터링에 대해서 테스트함
+        assertFalse(authHandler.authenticate(null));
+        assertFalse(authHandler.authenticate("Bearer anyToken"));
+    }
+
+    @Test
+    void authenticate_fail_credentials() {
+       // Base64의 형식이 맞지 않는 경우, : 빠져있음
+        String authHeader = "Basic " + Base64.getEncoder().encodeToString("B1ufferPassword123".getBytes());
+        when(userRepository.validate("B1uffer", "password123")).thenReturn(false);
+
+        // when
+        boolean result = authHandler.authenticate(authHeader);
+
+        // then
+        assertFalse(result);
     }
 }
