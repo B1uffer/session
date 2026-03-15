3752b3ab18390445958cb5e47c7cb34d22c6ed1b
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-15 15:41:02 +0900

    Security에 환경변수 주입 및 ClientRegistrationRepository 클래스 생성 Bean 등록 RegistrationRepository 구현

--- src/main/java/com/b1uffer/sessiontest/config/SecurityConfiguration.java
+++ src/main/java/com/b1uffer/sessiontest/config/SecurityConfiguration.java
@@ -1,14 +1,25 @@
 package com.b1uffer.sessiontest.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -21,5 +32,23 @@ public class SecurityConfiguration {
                 .oauth2Login(withDefaults());
 
         return http.build();
+    }
+
+    /**
+     * JAVA Configuration을 활용해서 OAUTH2 인증을 설정하기
+     */
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        var clientRegistration =  clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientRegistration);
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider
+                .GOOGLE
+                .getBuilder("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .build();
     }
 }
