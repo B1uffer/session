bf2110609965cbf695c0094459c7a1a85c111eb1
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-15 15:14:52 +0900

    로그 추가

--- src/main/java/com/b1uffer/sessiontest/controller/HomeController.java
+++ src/main/java/com/b1uffer/sessiontest/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.b1uffer.sessiontest.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomeController {
     @GetMapping("/hello-oauth2")
     public String home() {
+        log.info("OAuth 인증 감지");
         return "hello-oauth2";
     }
 }
