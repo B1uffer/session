ceb4ab6e710c94e2cbd8aea657cd77e7099d58c4
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-16 23:55:52 +0900

    UserService 클래스 생성

---
+++ src/main/java/com/b1uffer/sessiontest/service/UserService.java
@@ -0,0 +1,11 @@
+package com.b1uffer.sessiontest.service;
+
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@NoArgsConstructor
+public class UserService {
+}
