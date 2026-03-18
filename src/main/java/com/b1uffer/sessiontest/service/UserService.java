66b67f8495759945651d5b9bf53729e51a013d9b
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-19 00:44:57 +0900

    create 메서드 구현

--- src/main/java/com/b1uffer/sessiontest/service/UserService.java
+++ src/main/java/com/b1uffer/sessiontest/service/UserService.java
@@ -1,5 +1,7 @@
 package com.b1uffer.sessiontest.service;
 
+import com.b1uffer.sessiontest.entity.User;
+import com.b1uffer.sessiontest.repository.UserRepository;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,27 @@ import org.springframework.stereotype.Service;
 @Service
 @NoArgsConstructor
 public class UserService {
+    private UserRepository userRepository;
+
+    public User create(String username, int age) {
+        if(username == null) {
+            throw new IllegalArgumentException("Username cannot be null");
+        }
+
+        if(username.length() > 12) {
+            throw new IllegalArgumentException("Username cannot be longer than 12 characters");
+        }
+
+        if(age >= 123 || age < 0) {
+            throw new IllegalArgumentException("올바른 나이를 입력하세요.");
+        }
+
+        User user = new User(username, age);
+        userRepository.save(user);
+        return user;
+    }
+
+    public boolean validate(String username, String password) {
+        return "user".equals(username) && "password".equals(password);
+    }
 }
