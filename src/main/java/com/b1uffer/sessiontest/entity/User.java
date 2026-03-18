9ead883d040b2971580948909f6b2551367da0ac
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-18 23:28:49 +0900

    User 클래스에 칼럼 추가

--- src/main/java/com/b1uffer/sessiontest/entity/User.java
+++ src/main/java/com/b1uffer/sessiontest/entity/User.java
@@ -1,5 +1,6 @@
 package com.b1uffer.sessiontest.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,4 +19,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false, length = 12)
+    private String username;
+
+    @Column(nullable = false, length = 3)
+    private int age;
 }
