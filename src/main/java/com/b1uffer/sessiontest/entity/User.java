2eddee74fe70101199b62e2760602053a3bf83b2
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-20 06:53:00 +0900

    User import 정리

--- src/main/java/com/b1uffer/sessiontest/entity/User.java
+++ src/main/java/com/b1uffer/sessiontest/entity/User.java
@@ -3,8 +3,6 @@ package com.b1uffer.sessiontest.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
