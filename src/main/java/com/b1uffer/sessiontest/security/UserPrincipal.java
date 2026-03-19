b1b625f0df3f6923f88bf5bfdae4cc19085e9619
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-20 06:53:09 +0900

    UserPrincipal 구현

--- src/main/java/com/b1uffer/sessiontest/security/UserPrincipal.java
+++ src/main/java/com/b1uffer/sessiontest/security/UserPrincipal.java
@@ -1,4 +1,63 @@
 package com.b1uffer.sessiontest.security;
 
-public class UserPrincipal {
+import com.b1uffer.sessiontest.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                /**
+                 * permission 필드의 타입이 Set<String>이라서 flatMap을 사용함
+                 */
+                .flatMap(role -> role.getPermissions().stream())
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword(); // User의 password를 가져옴
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername(); // User의 username을 가져옴
+    }
 }
