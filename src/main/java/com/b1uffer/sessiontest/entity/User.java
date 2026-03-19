8bce283a13f3c9299d4f69ed0feb6e851f4fbcfa
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-20 04:51:52 +0900

    User 클래스 구현

--- src/main/java/com/b1uffer/sessiontest/entity/User.java
+++ src/main/java/com/b1uffer/sessiontest/entity/User.java
@@ -2,32 +2,50 @@ package com.b1uffer.sessiontest.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-@NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @Column(nullable = false, length = 12)
     private String username;
 
-    @Column(nullable = false, length = 3)
-    private int age;
+    @Column(nullable = false, length = 14)
+    private String password;
 
-    public User(String username, int age) {
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
         this.username = username;
-        this.age = age;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 }
