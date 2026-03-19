dfcda4293426ea5b692026d8a15fb883b3115ec8
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-20 03:58:50 +0900

    Role 클래스 구현

--- src/main/java/com/b1uffer/sessiontest/entity/Role.java
+++ src/main/java/com/b1uffer/sessiontest/entity/Role.java
@@ -1,4 +1,34 @@
 package com.b1uffer.sessiontest.entity;
 
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Role {
+    @Id
+    private String name; // ROLE_EDITOR, ROLE_VIEWER 등등..
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> permissions = new HashSet<>();
+
+    public Role() {
+
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<String> getPermissions() {
+        return permissions;
+    }
 }
