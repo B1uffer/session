d51b7e39441122b2f5caa915fe61655f2f3dacd2
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-20 07:04:31 +0900

    Post 클래스 생성

---
+++ src/main/java/com/b1uffer/sessiontest/entity/Post.java
@@ -0,0 +1,20 @@
+package com.b1uffer.sessiontest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+}
