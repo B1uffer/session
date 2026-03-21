c723ebbcb157c311eddcb45cb613579275f43af3
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-21 16:20:34 +0900

    getUser API 구현

--- src/main/java/com/b1uffer/sessiontest/controller/UserController.java
+++ src/main/java/com/b1uffer/sessiontest/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.b1uffer.sessiontest.controller;
 
+import com.b1uffer.sessiontest.entity.User;
+import com.b1uffer.sessiontest.security.UserPrincipal;
+import com.b1uffer.sessiontest.service.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private UserService userService;
+
+    @GetMapping("{username}")
+    public User getUser(@PathVariable String username, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+        if(!userPrincipal.getUsername().equals(username) || !userPrincipal.isAdmin()) {
+            throw new SecurityException("Invalid username or password");
+        }
+        User user = userService.get(username);
+        return user;
+    }
 }
