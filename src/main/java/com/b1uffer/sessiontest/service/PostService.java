9cc3dca18a65e614c44c0a3a1d7a095beddc57b7
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-21 14:24:50 +0900

    로그 추가

--- src/main/java/com/b1uffer/sessiontest/service/PostService.java
+++ src/main/java/com/b1uffer/sessiontest/service/PostService.java
@@ -4,10 +4,12 @@ import com.b1uffer.sessiontest.entity.Post;
 import com.b1uffer.sessiontest.repository.PostRepository;
 import com.b1uffer.sessiontest.security.UserPrincipal;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @NoArgsConstructor
 public class PostService {
@@ -28,9 +30,11 @@ public class PostService {
 
         // 작성자 본인이거나 관리자일 때 업데이트 가능
         if(!post.getOwner().equals(principal.getUsername()) || !principal.isAdmin()) {
+            log.info("게시글 수정 실패");
             throw new IllegalArgumentException("수정 권한이 없습니다.");
         }
         post.setContent(newContent);
+        log.info("게시글 수정 완료");
     }
 
     public void delete(Post post) {
@@ -40,9 +44,10 @@ public class PostService {
 
         // 작성자 본인이거나 관리자일 때 삭제 가능
         if(!post.getOwner().equals(principal.getUsername()) || !principal.isAdmin()) {
+            log.info("게시글 삭제 실패");
             throw new IllegalArgumentException("삭제 권한이 없습니다.");
         }
         postRepository.delete(post);
-        System.out.println("게시글 삭제 완료");
+        log.info("게시글 삭제 완료");
     }
 }
