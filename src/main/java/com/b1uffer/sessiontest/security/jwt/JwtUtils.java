8ab320a650dd770c49af7bca32661c269a624c4c
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-23 12:55:06 +0900

    크립토를 활용한 JwtUtils 예제의 parseToken 메서드 구현

--- src/main/java/com/b1uffer/sessiontest/security/jwt/JwtUtils.java
+++ src/main/java/com/b1uffer/sessiontest/security/jwt/JwtUtils.java
@@ -1,4 +1,20 @@
 package com.b1uffer.sessiontest.security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+
 public class JwtUtils {
+    private static final String SECRET = "mysecretkey123";
+    private static final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public static Claims parseToken(String token) {
+        return Jwts.parser()
+                .verifyWith(KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
