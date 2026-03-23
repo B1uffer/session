e84841288ddfe319e2eadad1857dcdb11121867a
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-23 12:21:13 +0900

    updatePost 메서드 구현

--- src/main/java/com/b1uffer/sessiontest/service/PostService.java
+++ src/main/java/com/b1uffer/sessiontest/service/PostService.java
@@ -1,8 +1,10 @@
 package com.b1uffer.sessiontest.service;
 
 import com.b1uffer.sessiontest.entity.Post;
+import com.b1uffer.sessiontest.entity.User;
 import com.b1uffer.sessiontest.repository.PostRepository;
 import com.b1uffer.sessiontest.security.UserPrincipal;
+import jakarta.servlet.http.HttpSession;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -36,6 +38,15 @@ public class PostService {
         }
         post.setContent(newContent);
         log.info("게시글 수정 완료");
+    }
+
+    // HttpSession을 활용한 권한 확인 예시
+    public void updatePost(HttpSession session, Post post, String newContent) {
+        User user = (User) session.getAttribute("user");
+        if(!post.getOwner().equals(user.getUsername()) && !user.getRoles().contains("ADMIN")) {
+            throw new SecurityException("수정 권한이 없습니다.");
+        }
+        post.setContent(newContent);
     }
 
     public void delete(Post post) {
