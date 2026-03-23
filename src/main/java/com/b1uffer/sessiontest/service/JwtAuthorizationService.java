13542eba936866af64eb543d5e5b32735bac24d1
B1uffer <135810601+B1uffer@users.noreply.github.com>
2026-03-23 12:55:55 +0900

    JWT를 활용한 updateContent 메서드 / 콘텐츠 업데이트 로직 구현

--- src/main/java/com/b1uffer/sessiontest/service/JwtAuthorizationService.java
+++ src/main/java/com/b1uffer/sessiontest/service/JwtAuthorizationService.java
@@ -1,4 +1,26 @@
 package com.b1uffer.sessiontest.service;
 
+import com.b1uffer.sessiontest.entity.Post;
+import com.b1uffer.sessiontest.security.jwt.JwtUtils;
+import io.jsonwebtoken.Claims;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@NoArgsConstructor
 public class JwtAuthorizationService {
+    public void updateContent(String jwtToken, Post post, String newContent) {
+        Claims claims = JwtUtils.parseToken(jwtToken);
+        String userId = claims.getSubject();
+        List<String> roles = claims.get("roles", List.class);
+
+        if(!post.getOwner().equals(userId) && !roles.contains("ADMIN")) {
+            throw new SecurityException("수정 권한이 없습니다.");
+        }
+        post.setContent(newContent);
+    }
 }
